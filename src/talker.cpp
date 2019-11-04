92f146fae59bfe7df5beac38e807b4cf95199b1e
Ashwin Varghese Kuruttukulam <ashwinvk94@gmail.com>
2019-11-04 14:47:13 -0500

    Add Doxygen style comments

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,40 +1,15 @@
-/*
- * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ /**
+ @file talker.cpp
+ @author Ashwin Varghese Kuruttukulam
+ @copyright MIT
+ @brief ROS subscriber publishes to  to chatter topic 
+ @brief ROS publisher that publishes to chatter topic
+ ROS publisher from the ros tutorials page 
  */
-// %Tag(FULLTEXT)%
-// %Tag(ROS_HEADER)%
-
 #include <sstream>
 #include "ros/ros.h"
-// %EndTag(ROS_HEADER)%
-// %Tag(MSG_HEADER)%
 #include "std_msgs/String.h"
 #include "beginner_tutorials/customString.h"
-// %EndTag(MSG_HEADER)%
-
 #include "beginner_tutorials/editText.h"
 
 std::string defaultMessage = "This is the defult message";
@@ -60,17 +35,13 @@ int main(int argc, char **argv) {
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
-// %Tag(INIT)%
   ros::init(argc, argv, "talker");
-// %EndTag(INIT)%
   /**
    * NodeHandle is the main access point to communications with the ROS system.
    * The first NodeHandle constructed will fully initialize this node, and the last
    * NodeHandle destructed will close down the node.
    */
-// %Tag(NODEHANDLE)%
   ros::NodeHandle n;
-// %EndTag(NODEHANDLE)%
 
   /**
    * The advertise() function is how you tell ROS that you want to
@@ -89,14 +60,10 @@ int main(int argc, char **argv) {
    * than we can send them, the number here specifies how many messages to
    * buffer up before throwing some away.
    */
-// %Tag(PUBLISHER)%
   ros::Publisher chatter_pub =
 n.advertise<beginner_tutorials::customString>("chatter", 1000);
-// %EndTag(PUBLISHER)%
 
-// %Tag(LOOP_RATE)%
   ros::Rate loop_rate(10);
-// %EndTag(LOOP_RATE)%
 
   /**
    * A count of how many messages we have sent. This is used to create
@@ -104,48 +71,33 @@ n.advertise<beginner_tutorials::customString>("chatter", 1000);
    */
 
   ros::ServiceServer server = n.advertiseService("changeText", changeMessage);
-// %Tag(ROS_OK)%
   int count = 0;
   ROS_DEBUG_STREAM("Starting string publisher");
   while (ros::ok()) {
-// %EndTag(ROS_OK)%
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
-// %Tag(FILL_MESSAGE)%
     beginner_tutorials::customString msg;
 
     std::stringstream ss;
     ss << defaultMessage << count;
     msg.data.data = ss.str();
-// %EndTag(FILL_MESSAGE)%
-
-// %Tag(ROSCONSOLE)%
     ROS_INFO("%s", msg.data.data.c_str());
-// %EndTag(ROSCONSOLE)%
-
     /**
      * The publish() function is how you send messages. The parameter
      * is the message object. The type of this object must agree with the type
      * given as a template parameter to the advertise<>() call, as was done
      * in the constructor above.
      */
-// %Tag(PUBLISH)%
     chatter_pub.publish(msg);
-// %EndTag(PUBLISH)%
 
-// %Tag(SPINONCE)%
     ros::spinOnce();
-// %EndTag(SPINONCE)%
 
-// %Tag(RATE_SLEEP)%
     loop_rate.sleep();
-// %EndTag(RATE_SLEEP)%
     ++count;
   }
 
   ROS_FATAL_STREAM("ROS Node has died");
   return 0;
 }
-// %EndTag(FULLTEXT)%
 
